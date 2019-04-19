2b6aa6d206a556dd08e9dccb7f57832536185d85
Baldi <baldilp@gmail.com>
2019-04-19 19:38:57 -0300

    Upgrade react-native version

--- __tests__/components/Button.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Button from '../../src/components/Button'
-import renderer from 'react-test-renderer'
-
-test('should render button', () => {
-  const buttonTree = renderer.create(<Button label='Potatoe' />).toJSON()
-  expect(buttonTree).toMatchSnapshot()
-})
